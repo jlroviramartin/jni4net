07087f93edb1be827b076f1ce08d5b6d89ed55f1
Jose Luis R. M <jlroviramartin@gmail.com>
2019-08-16 20:32:05 +0200

    Restructured

--- generate.bat
+++ generate.bat
@@ -4,7 +4,9 @@ setlocal
 set PATH=C:\Aplicaciones\Desarrollo\swigwin\4.0.0;%PATH%
 set JNI_PATH=C:\Program Files\Java\jdk-1.8\include
 
-mkdir output
-swig.exe -verbose -csharp -c++ -I"\win32" -v -outdir output\ java_jni.i
+mkdir Jni\Generated\
+mkdir Jni\win64\
+swig.exe -verbose -help
+swig.exe -verbose -csharp -c++ -I"\swig" -I"\swig\win32" -v -outdir Jni\Generated\ -o Jni\win64\jni4net.cxx -oh Jni\win64\jni4net.h swig\java_jni.i
 
 endlocal
